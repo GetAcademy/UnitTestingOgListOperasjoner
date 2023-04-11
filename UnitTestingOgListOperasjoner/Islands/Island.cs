2d4f89e3cec5e9a2ee7d4b6e9f9b7fe409990ddf
GET Academy <terje@getacademy.no>
2023-04-11 09:43:57 +0200

    Initial commit

---
+++ UnitTestingOgListOperasjoner/Islands/Island.cs
@@ -0,0 +1,18 @@
+﻿namespace UnitTestingOgListOperasjoner.Islands
+{
+    internal class Island
+    {
+        private int _rank;
+        private string _name;
+        private int _area;
+        private string[] _countries;
+
+        public Island(int rank, string name, int area, string countries)
+        {
+            _countries = countries.Split(',', ' ');
+            _area = area;
+            _name = name;
+            _rank = rank;
+        }
+    }
+}
---
+++ UnitTestingOgListOperasjoner/Program.cs
@@ -0,0 +1,8 @@
+﻿/*
+ * 1: Kort eksempel unit teste klasse
+ * 2: Tre ulike perspektiver på unit testing
+ *    - Kvalitet, utviklingshastighet
+ *    - Testdrevet utvikling
+ *    - Test coverage
+ * 3: Operasjoner på lister av objekter med og uten LINQ, via unit testing
+ */
